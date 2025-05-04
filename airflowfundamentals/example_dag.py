c9f4c9232211b416b232dec8ba6ba23548741d47
KofiAmoakoMensah <kofiamoakomensa@icloud.com>
2025-05-04 08:33:45 +0100

    changes to airflow fundamentals

--- airflowfundamentals/example_dag.py
+++ airflowfundamentals/example_dag.py
@@ -9,6 +9,9 @@ from datetime import datetime, timedelta # for date and time manipulation
 def hello_airflow():
     print("Hello Airflow from Astro CLI!")
 
+def howare_you():
+    print("How are you doing today?") 
+
 # default args dictionary holds paramaters that apply to all tasks within the DAG 
 default_args = {
     'owner': 'airflow', # owner of the task 
@@ -20,7 +23,7 @@ default_args = {
 
 # DAG CONTEXT 
 with DAG(
-    'simple_astro_dag', # unique identifier for the DAG
+    dag_id = 'simple_astro_dag', # unique identifier for the DAG
     default_args=default_args, # applied to all tasks in the DAG 
     description='A simple DAG using Astro CLI', # what the dag does 
     schedule_interval=timedelta(days=1), # controls how often the DAG is triggered 
@@ -31,3 +34,12 @@ with DAG(
         task_id='hello_task', # gives the task a unique id 
         python_callable=hello_airflow, # tells airflow to execute the hello_airflow function when the task is run
     )
+
+    # another task is created using the PythonOperator 
+    howareyou_task = PythonOperator(
+        task_id='howareyou_task', # gives the task a unique id 
+        python_callable=howare_you, # tells airflow to execute the howare_you function when the task is run
+    )
+    # set the task dependencies
+    # this means that the hello_task must be completed before howareyou_task can start
+    hello_task >> howareyou_task 
